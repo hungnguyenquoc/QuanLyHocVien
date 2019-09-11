b08d1b4272d4db42dc4a79f720f648db5542d5f6
Nguyen Quoc Hung <hungnguyen.1614@gmail.com>
2019-09-11 10:45:34 +0700

    add controller

---
+++ QuanLyHocVien/QuanLyHocVien.Web/Controllers/aController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyHocVien.Web.Controllers
+{
+    public class aController : Controller
+    {
+        // GET: a
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
